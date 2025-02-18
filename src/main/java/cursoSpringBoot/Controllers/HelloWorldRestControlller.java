a8b53031a298da8b33f5153ddbb7293b0dcdc9b7
Aldahir Zamora <ronn.zp.3@gmail.com>
2025-02-17 23:55:03 -0500

    path variable

--- src/main/java/cursoSpringBoot/Controllers/HelloWorldRestControlller.java
+++ src/main/java/cursoSpringBoot/Controllers/HelloWorldRestControlller.java
@@ -1,15 +1,16 @@
 package cursoSpringBoot.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldRestControlller {
 
-    @GetMapping({"/hello", "/hw", "/hola"})
-    public String helloWorld() {
+    @GetMapping("/hello/{name}")
+    public String helloWorld(@PathVariable String name) {
         System.out.println("Solicitud ejecutada");
-        return "Hola, Mundo";
+        return "Hola, " + name;
     }
 
 }
